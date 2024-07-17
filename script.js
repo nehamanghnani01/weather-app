62e4fe740593f42db08355bd896006d0894b7920
Neha <nehajm@tamu.edu>
2024-07-16 21:03:19 -0500

    search city added

--- script.js
+++ script.js
@@ -1,15 +1,47 @@
-const APIKEY = "<add_key_here>";
-const APIURL = `https://api.openweathermap.org/data/2.5/weather?q=bangalore&appid=${APIKEY}&units=metric`;
+const APIKEY = "dd934d2fb0d5219fd561d8fff414f34f";
+const APIURL = `https://api.openweathermap.org/data/2.5/weather?appid=${APIKEY}&units=metric&q=`;
+const searchbox = document.querySelector(".search input");
+const searchbutton = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 
-async function checkWeather() {
-    const response = await fetch(APIURL);
-    var data = await response.json();
-    console.log(data);
+async function checkWeather(cityName) {
+    const response = await fetch(APIURL+cityName);
 
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
-    document.querySelector(".humidity").innerHTML = data.main.humidity+"%";
-    document.querySelector(".wind").innerHTML = data.wind.speed+ " km/h";
+    if(response.status == 404) {
+        document.querySelector(".weather").style.display = "none";
+        document.querySelector(".error").style.display = "block";
+        return;
+    } else {
+        document.querySelector(".error").style.display = "none";
+        document.querySelector(".weather").style.display = "block";
+    
+
+        var data = await response.json();
+        console.log(data);
+
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity+"%";
+        document.querySelector(".wind").innerHTML = data.wind.speed+ " km/h";
+
+        if(data.weather[0].main == "Clouds") {
+            weatherIcon.src = "images/clouds.png";
+        } else if(data.weather[0].main == "Rain") {
+            weatherIcon.src = "images/rain.png";
+        } else if(data.weather[0].main == "Clear") {    
+            weatherIcon.src = "images/clear.png";
+        } else if(data.weather[0].main == "Mist") {
+            weatherIcon.src = "images/mist.png";
+        } else if(data.weather[0].main == "Snow") {
+            weatherIcon.src = "images/snow.png";
+        } else if(data.weather[0].main == "Drizzle") {
+            weatherIcon.src = "images/drizzle.png";
+        }
+    }
+    
 }
 
-checkWeather();
+searchbutton.addEventListener("click", () => {
+    console.log("search clicked!")
+    checkWeather(searchbox.value);
+});
